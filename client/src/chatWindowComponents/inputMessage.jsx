20dae90c524c05ec20aaf65ef48a82ce855b42e1
Leonid Lichkun <lionproklon@gmail.com>
2022-01-06 01:09:15 +0300

    Added sendig message with Ctrl+Enter

--- client/src/chatWindowComponents/inputMessage.jsx
+++ client/src/chatWindowComponents/inputMessage.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const InputMessage = ({ value, setValue, sendMessage }) => {
+  const sendMessageKeyDown = (event) => {
+    if (event.code === "Enter" && event.ctrlKey) {
+      event.preventDefault();
+      event.stopPropagation();
+      sendMessage();
+    }
+  };
   return (
     <div className="footer">
       <div className="input-group mb-3">
@@ -12,6 +19,7 @@ const InputMessage = ({ value, setValue, sendMessage }) => {
           className="form-control"
           placeholder="Введите здесь сообщение"
           value={value}
+          onKeyDown={sendMessageKeyDown}
           onChange={(e) => setValue(e.target.value)}
         />
         <button type="submit" className="btn btn-primary" onClick={sendMessage}>
